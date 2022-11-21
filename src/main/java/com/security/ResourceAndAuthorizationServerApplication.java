6237412aa43892865d2ad01efe8a6a3e471ecdf6
Tiago Silva <tiagosp13@gmail.com>
2022-11-20 22:34:12 -0300

    Adicionando lib do flyway

--- src/main/java/com/security/ResourceAndAuthorizationServerApplication.java
+++ src/main/java/com/security/ResourceAndAuthorizationServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResourceServerApplication {
+public class ResourceAndAuthorizationServerApplication {
     
     public static void main(String[] args) {
-        SpringApplication.run(ResourceServerApplication.class, args);
+        SpringApplication.run(ResourceAndAuthorizationServerApplication.class, args);
     }
     
 }
